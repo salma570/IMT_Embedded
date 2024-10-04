4a9c4c9ee0522af4dd7dc1ac435d711caf47dd25
Amira Abdelghany <amira.say.yes@gmail.com>
2024-10-04 13:56:15 +0200

    added push button

--- FinalProject/main.c
+++ FinalProject/main.c
@@ -51,7 +51,7 @@ void main(void)
 
 			break;
 		case StopWatch:
-			DIO_SetPinDirection(PORTD,PIN_2,Input);
+			DIO_SetPinDirection(PORTD,PIN_2,Input); //push button connected to exti0
 			DIO_SetPinValue(PORTD,PIN_2,HIGH); //pullup
 
 			break;
